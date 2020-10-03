c4d65d2b0efc65fd8fe23492bf06cfc189eca4f5
Ankit Kumar <kumar.ankit383@gmail.com>
2020-10-04 01:13:27 +0530

    Private route added

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import {Redirect, Route} from 'react-router-dom';
 
@@ -7,6 +7,26 @@ const PrivateRoute = ({isloggedin, component : Component, ...rest}) => {
         <Route
         {...rest}
         component = {(props)=>{return isloggedin ? <Component {...props} isloggedin={isloggedin} /> : <Redirect to="/login" {...props} /> }}
+        />
+    )
+}
+
+const myPrivateRoute = ({isloggedin, component : Component, ...rest}) => {
+    return (
+        <Route
+        {...rest}
+        component={
+            (props) => (
+                isloggedin ? 
+                <Component 
+                {...props}
+                isloggedin={isloggedin}
+                />
+                : <Redirect 
+                path='/login'
+                />
+            )
+        }
         />
     )
 }
